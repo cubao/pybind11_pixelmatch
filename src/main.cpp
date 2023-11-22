4f674c263ad3f4c32a8b5a11a73489cd8a27d6dc
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-11-22 23:53:59 +0800

    add packaging (migrate from pybind11_coordtransform)

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+PYBIND11_MODULE(_core, m)
+{
+    m.doc() = R"pbdoc(
+    )pbdoc";
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
