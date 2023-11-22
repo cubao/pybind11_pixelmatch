64d86917388b23aff53664d2cb7bf88f61f3608d
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-11-23 00:19:03 +0800

    bind color

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <pixelmatch/pixelmatch.h>
 #include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
@@ -9,6 +10,36 @@ using namespace pybind11::literals;
 PYBIND11_MODULE(_core, m) {
   m.doc() = R"pbdoc(
     )pbdoc";
+
+  using Color = pixelmatch::Color;
+  py::class_<Color>(m, "Color", py::module_local())  //
+      .def(py::init<>())
+      .def(py::init<uint8_t, uint8_t, uint8_t, uint8_t>(), "r"_a, "g"_a, "b"_a, "a"_a)
+      .def_readwrite("r", &Color::r)
+      .def_readwrite("g", &Color::g)
+      .def_readwrite("b", &Color::b)
+      .def_readwrite("a", &Color::a)
+      .def(
+          "from_python",
+          [](Color& self, const std::vector<uint8_t>& rgba) {
+            self.r = rgba[0];
+            self.g = rgba[1];
+            self.b = rgba[2];
+            self.a = rgba[3];
+          },
+          "rgba"_a)
+      .def("to_python",
+           [](const Color& self) -> std::vector<uint8_t> {
+             return {self.r, self.g, self.b, self.a};
+           })
+      //
+      ;
+
+  // using Options = pixelmatch::Options;
+  //  py::class_<Options>(m, "Options", py::module_local()) //
+  //     .def(py::init<>())
+  //     //
+  //     ;
 
 #ifdef VERSION_INFO
   m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
